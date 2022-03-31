0f6420b8c2b482853c47d580c283b27ce51a75ba
gabriel <gabriel_aq7@hotmail.com>
2022-03-31 14:18:13 -0300

    Initial commit

---
+++ src/test/java/dev/gabrielQ7/streamsschedule/StreamsScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielQ7.streamsschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/dev/gabrielQ7/streamsschedule/model/MutableLiveStreamTest.java
@@ -0,0 +1,44 @@
+package dev.gabrielQ7.streamsschedule.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MutableLiveStreamTest {
+
+    @Test
+    void create_new_mutable_live_stream() {
+        MutableLiveStream stream = new MutableLiveStream();
+        stream.setId(UUID.randomUUID().toString());
+        stream.setTitle("Building REST APIs with Spring Boot");
+        stream.setDescription("""
+                Spring Boot is very conveient to use when building REST APIs;
+                """);
+        stream.setUrl("https://www.twitch.tv/danvega");
+        stream.setStartDate(LocalDateTime.of(2022,2,16,11,0));
+        stream.setStartDate(LocalDateTime.of(2022,2,16,12,0));
+
+        assertNotNull(stream);
+        assertEquals("Building REST APIs with Spring Boot", stream.getTitle());
+    }
+
+    @Test
+    void create_new_imutable_live_stream() {
+        ImmutableLiveStream stream = new ImmutableLiveStream(
+                UUID.randomUUID().toString(),
+                "Building REST APIs with Spring Boot",
+                """
+                Spring Boot is very conveient to use when building REST APIs;
+                """,
+                "https://www.twitch.tv/danvega",
+                LocalDateTime.of(2022,2,16,11,0),
+                LocalDateTime.of(2022,2,16,12,0)
+        );
+        assertNotNull(stream);
+        assertEquals("Building REST APIs with Spring Boot", stream.getTitle());
+    }
+
+}
